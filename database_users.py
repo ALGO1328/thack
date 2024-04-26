c953e905f539d9336a6bb9c8d1af2a204effe7c0
ekcne <ekcnert1337@icloud.com>
2024-04-26 23:12:46 +0300

    bugfix

--- database_users.py
+++ database_users.py
@@ -8,48 +8,48 @@ import telebot.types
 
 logs = open('logs.txt', 'w')
 
-now = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
+
 
 conn = sqlite3.connect('database.db')
 
+
 def database_connect() -> bool:
+    now = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
     try:
         conn = sqlite3.connect('database.db')
-        logs.write('Database connection SUCCESS' + now.strftime("%H:%M:%S") + '\n')
+        logs.write(now.strftime("%H:%M:%S") + 'Успешно подключено' + '\n')
         return True
     except sqlite3.Error as e:
-        logs.write('Database connection FAIL' + now.strftime("%H:%M:%S") + '\n')
+        logs.write(now.strftime("%H:%M:%S") + 'Ошибка подключения к базе' + '\n')
         return False
 
+
 def check_user_exist(userinfo: telebot.types.Message) -> bool:
-    try:
-        c = conn.cursor()
-        c.execute("SELECT id FROM users")
-        user_id = c.fetchall()
-        for id in user_id:
-            if id == userinfo.chat.id:
-                logs.write('User exists' + now.strftime("%H:%M:%S") + '\n')
-                return True
-    except sqlite3.Error as e:
-        logs.write('No user found' + now.strftime("%H:%M:%S") + '\n')
+    c = conn.cursor()
+    c.execute(f"SELECT id FROM users WHERE id={userinfo.chat.id}")
+    user_id = c.fetchall()
+    if user_id:
+        return True
+    else:
         return False
+
 
 def register_user(userinfo: telebot.types.Message) -> bool:
     try:
         c = conn.cursor()
-        c.execute("INSERT INTO users (id, name) VALUES (userinfo.chat.id, userinfo.from_user.username)")
-        logs.write(now.strftime("%H:%M:%S") + '\n')
+        c.execute(f"INSERT INTO users (id, name) VALUES (?, ?)",
+                  (userinfo.chat.id, userinfo.from_user.username))
+        conn.commit()
         return True
     except sqlite3.Error as e:
         return False
+
 
 def add_meeting(creator, time, URL) -> bool:
     try:
         c = conn.cursor()
-        c.execute("INSERT INTO meetings (time, creator, URL) VALUES (time, creator, URL)")
-        logs.write(now.strftime("%H:%M:%S") + '\n')
+        c.execute("INSERT INTO meetings (time, creator, URL) VALUES (?, ?, ?)", (time, creator, URL))
+        conn.commit()
         return True
     except sqlite3.Error as e:
         return False
-
-
