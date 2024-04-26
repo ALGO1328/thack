e60e4aef37dce120f2f3c7c7a22915e998ff5c42
BuildTools <unconfigured@null.spigotmc.org>
2024-04-26 22:03:59 +0300

    pizdos

--- database_users.py
+++ database_users.py
@@ -8,6 +8,8 @@ import telebot.types
 
 logs = open('logs.txt', 'w')
 
+#TEMP
+
 
 def database_connect() -> bool:
     try:
@@ -16,8 +18,22 @@ def database_connect() -> bool:
     except sqlite3.Error as e:
         return False
 
+''' DATABASE CREATOR
+def create_database():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE users
+                 (id TEXT PRIMARY KEY NOT NULL,
+                  name TEXT NOT NULL)''')
+
+    c.execute('''CREATE TABLE meetings
+                (time DATE PRIMARY KEY NOT NULL,
+                creator TEXT NOT NULL, 
+                URL TEXT NOT NULL)''')
+'''
 
 def add_member_to_base(userinfo: telebot.types.Message) -> bool:
+    c = conn.cursor()
     '''+ к базе userinfo.chat.id - id пользователя
     userinfo.from_user.username - юзернейм
     '''
@@ -35,6 +51,4 @@ def register_user(userinfo: telebot.types.Message) -> bool:
 
 def add_schedule(userinfo: telebot.types.Message) -> bool:
     pass
-
-
-...
+create_database()
