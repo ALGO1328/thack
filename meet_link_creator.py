b1a16076cf5bd91f91d7b63ddbd2e0cb77ed2751
ekcne <ekcnert1337@icloud.com>
2024-04-28 00:07:59 +0300

    refactoring

--- meet_link_creator.py
+++ meet_link_creator.py
@@ -8,10 +8,13 @@ import jwt
 
 import requests
 
+from sys import getdefaultencoding
+
 import json
 
-import dateconv
+from time import time
 
+import dateconv
 
 # Enter your API key and your API secret
 API_KEY = 'ffBmOzGDQpG6ReFkpyVALw'
@@ -20,57 +23,54 @@ API_SEC = 'Oi_STeQoQPu6d885cV4KCA'
 
 def generateToken():
     token = jwt.encode(
-
-        # Create a payload of the token containing
-        # API Key & expiration time
         {'iss': API_KEY, 'exp': time() + 5000},
-
-        # Secret used to generate token signature
         API_SEC,
-
-        # Specify the hashing alg
         algorithm='HS256'
     )
-    return token.decode('utf-8')
+    print(jwt.decode(token, API_SEC, algorithms=["HS256"]))
+    return jwt.decode(token, API_SEC, algorithms=["HS256"])
 
 
 # create json data for post requests
-def meetingdetails(creator: str,time: int ):
+def meetingdetails(creator: str, time: int):
     meetingdetails = {"topic": f"Встреча {creator}",
-                    "type": 2,
-                    "start_time": f"{dateconv.from_unix(time)}",
-                    "duration": "45",
-                    "timezone": "Europe/Moscow",
-                    "agenda": "",
+                      "type": 2,
+                      "start_time": f"{dateconv.from_unix(time)}",
+                      "duration": "45",
+                      "timezone": "Europe/Moscow",
+                      "agenda": "",
 
-                    "recurrence": {"type": 1,
-                                 "repeat_interval": 1
-                                 },
-                    "settings": {"host_video": "true",
-                                "participant_video": "true",
-                                "join_before_host": "true",
-                                "mute_upon_entry": "False",
-                                "watermark": "true",
-                                "audio": "voip",
-                                "auto_recording": "cloud"
-                               }
-                  }
+                      "recurrence": {"type": 1,
+                                     "repeat_interval": 1
+                                     },
+                      "settings": {"host_video": "true",
+                                   "participant_video": "true",
+                                   "join_before_host": "true",
+                                   "mute_upon_entry": "False",
+                                   "watermark": "true",
+                                   "audio": "voip",
+                                   "auto_recording": "cloud"
+                                   }
+                      }
     return meetingdetails
 
 
-def createMeeting(creator, time):
-    headers = {'authorization': 'Bearer ' + generateToken(),
+def createMeeting(creator, time1):
+    headers = {'authorization': f'Bearer  + {generateToken()['iss']}',
                'content-type': 'application/json'}
     r = requests.post(
         f'https://api.zoom.us/v2/users/me/meetings',
-        headers=headers, data=json.dumps(meetingdetails(creator, time)))
+        headers=headers, data=json.dumps(meetingdetails(creator, time1)))
 
     print("\n creating zoom meeting ... \n")
     # print(r.text)
     # converting the output into json and extracting the details
     y = json.loads(r.text)
+    print(y)
     join_URL = y["join_url"]
     meetingPassword = y["password"]
 
     return join_URL, meetingPassword
 
+
+print(createMeeting('@algo1328', 1714244140))
