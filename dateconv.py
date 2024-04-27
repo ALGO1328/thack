8d087618264d0086bb7cc2e9065d7887ef1446e4
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 17:02:46 +0300

    added from_unix func nd added future check to check_date func

--- dateconv.py
+++ dateconv.py
@@ -3,30 +3,41 @@ import time
 
 logs = open('logs.txt', 'w')
 
+now = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
+
 def checkdate(timeinfo) -> bool:
     for t in timeinfo:
         if t.count(':') != 0:
-            time  = t.split(':')
+            time = list(map(str, i.split(':')))
         else:
-            date = t.split('.')
+            date = list(map(str, i.split('.')))
 
 
     if len(date) == 3:
         year = date[2]
     else:
-        year = datetime.date.today().year
-    month = date[1]
-    day = date[0]
+        year = datetime.date.today.().year
+    month = int(date[1])
+    day = int(date[0])
 
-    hour = time[0]
-    minute = time[1]
+    hour = int(time[0])
+    minute = int(time[1])
 
     try:
         newDate = datetime.datetime(year, month, day, hour, minute)
-        correctDate = True
+        correct_date = True
     except ValueError:
-        correctDate = False
-    return correctDate
+        correct_date = False
+
+    if datetime.datetime(year, month, day, hour, minute) - datetime.datetime.now() >= 0 :
+        is_future = True
+    else:
+        is_future = False
+
+    return is_future and correct_date
+
+
+
 
 
 def to_unix(timeinfo) -> float:
@@ -51,6 +62,8 @@ def to_unix(timeinfo) -> float:
     return (dt - datetime.datetime(1970,1,1)).total_seconds()
 
 
-#def from_unix(unixtime) -> list:
+def from_unix(unixtime):
+    return datetime.datetime.fromtimestamp(unixtime)
+
 
 
