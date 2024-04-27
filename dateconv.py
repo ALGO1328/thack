afd877d358aa62ee2b4b7c97d3973e74d15fcdab
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 15:06:23 +0300

    added to_unix func

--- dateconv.py
+++ dateconv.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 logs = open('logs.txt', 'w')
 
@@ -28,10 +29,28 @@ def checkdate(timeinfo) -> bool:
     return correctDate
 
 
+def to_unix(timeinfo) -> float:
+    for t in timeinfo:
+        if t.count(':') != 0:
+            time = t.split(':')
+        else:
+            date = t.split('.')
 
 
+    if len(date) == 3:
+        year = date[2]
+    else:
+        year = datetime.date.today().year
+    month = date[1]
+    day = date[0]
+
+    hour = time[0]
+    minute = time[1]
+
+    dt = datetime.datetime(year, month, day, hour, minute)
+    return (dt - datetime.datetime(1970,1,1)).total_seconds()
 
 
-def tounix(timeinfo) -> int:
+#def from_unix(unixtime) -> list:
 
 
