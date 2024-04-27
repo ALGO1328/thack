486a6c6ee3e06cbd18775667d045686d9688e52a
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 19:39:02 +0300

    bugfix

--- meetings.py
+++ meetings.py
@@ -14,25 +14,25 @@ class Meeting:
         self.creator = None
         self.members = None
 
-    def add_link(self, link):
+    def add_link(self, link: str):
         self.link = link
 
-    def add_time(self, time):
+    def add_time(self, time: int):
         self.time = time
 
-    def add_creator(self, creator):
+    def add_creator(self, creator: str):
         self.creator = creator
 
     def add_members(self, members):
         self.members = members
 
-    def get_info(self) -> str:  # Вас прглашает на встречу .. .. . (через
+    def get_info(self) -> str:
         event_time = dateconv.from_unix(self.time)
         dt = datetime.datetime.now(pytz.timezone('Europe/Moscow'))
         unix_now = int((dt - datetime.datetime(1970, 1, 1)).total_seconds())
         time_to_event = str(datetime.timedelta(seconds=self.time - unix_now))
 
-        return (f'Вас приглашает на встречу {self.creator}, '
-                f'дата: {datetime.datetime.fromtimestamp(self.time, datetime.UTC).strftime('%d.%m.%Y')} '
+        return (f'{self.creator} приглашает вас на встречу, '
+                f'дата: {datetime.date(event_time).strftime("%d.%m.%y")}, '
                 f'в {datetime.datetime(event_time).strftime("%H:%M")}' 
                 f'(через: {time_to_event} )')
