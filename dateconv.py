48875f437f67e5015444a35716715497e421a3ab
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 14:54:20 +0300

    1

--- dateconv.py
+++ dateconv.py
@@ -1,1 +1,37 @@
-pass
+import datetime
+
+logs = open('logs.txt', 'w')
+
+def checkdate(timeinfo) -> bool:
+    for t in timeinfo:
+        if t.count(':') != 0:
+            time  = t.split(':')
+        else:
+            date = t.split('.')
+
+
+    if len(date == 3):
+        year = date[2]
+    else:
+        year = datetime.date.today().year
+    month = date[1]
+    day = date[0]
+
+    hour = time[0]
+    minute = time[1]
+
+    try:
+        newDate = datetime.datetime(year, month, day, hour, minute)
+        correctDate = True
+    except ValueError:
+        correctDate = False
+    return correctDate
+
+
+
+
+
+
+def tounix(timeinfo) -> int:
+
+
