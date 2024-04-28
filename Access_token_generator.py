366d19b72799b31e20f503b6fb9b1ecb4f2a91ee
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 11:51:30 +0300

    refactoring

--- Access_token_generator.py
+++ Access_token_generator.py
@@ -1,12 +1,13 @@
 from pyzoom import *
 import datetime
+import json
 import pytz
 from flask import Flask, abort, request
 from uuid import uuid4
 import requests
 import requests.auth
 import urllib
-
+"""
 CLIENT_ID = "ghwuvh95Rd2tLtfzA1s_0w"  # Fill this in with your client ID
 CLIENT_SECRET = "CfI2vDSgbiIo8hpELrOuN1O1CQfPgP0sT"  # Fill this in with your client secret
 REDIRECT_URI = "http://localhost:65010/integrations/zoom"
@@ -67,9 +68,10 @@ def get_username(access_token):
 
 if __name__ == '__main__':
     app.run(debug=True, port=65010)
-'''
+
 now = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
 
+"""
 CLIENT_ID = 'ghwuvh95Rd2tLtfzA1s_0w'
 CLIENT_SECRET = 'CfI2vDSgbiIo8hpELrOuN1O1CQfPgP0s'
 
@@ -79,5 +81,47 @@ ZOOM_ACCESS_TOKEN = tokens['access_token']
 
 client = ZoomClient('ZOOM_ACCCESS_TOKEN', base_url="https://api.zoom.us/v2")
 
-meeting = client.meetings.create_meeting('Auto created 1', start_time=datetime.datetime.now().isoformat(), duration_min=60, password='not-secure)
-'''
+creator = "Damir"
+start_time = datetime.datetime(2024, 4, 30, 10, 0)
+
+def createMeeting(creator, start_time):
+    headers = {
+        'authorization': f"{tokens}",
+        'content-type': 'application/json'
+    }
+
+    meeting_details = {
+        "topic": f"Meeting created by {creator}",
+        "type": 2,
+        "start_time": f"{start_time.strftime('%Y-%m-%dT%H:%M:%SZ')}",
+        "duration": "60",
+        "timezone": "Europe/Moscow",
+        "agenda": "",
+        "recurrence": {"type": 1, "repeat_interval": 1},
+        "settings": {
+            "host_video": "true",
+            "participant_video": "true",
+            "join_before_host": "true",
+            "mute_upon_entry": "False",
+            "watermark": "false",
+            "audio": "voip",
+            "auto_recording": "cloud"
+        }
+    }
+
+    r = requests.get(
+        'https://api.zoom.us/v2/users/me/meetings',
+        headers=headers,
+        data=json.dumps(meeting_details)
+    )
+
+    if r.status_code == 201:
+        response_data = r.json()
+        join_url = response_data.get("join_url")
+        password = response_data.get("password")
+        return join_url, password
+    else:
+        print("Error creating Zoom meeting:", r.text)
+        return None, None
+
+createMeeting(creator, start_time)
